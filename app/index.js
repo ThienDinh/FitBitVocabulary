48b127980789f6253feda84fdee7176b33e3ac4b
Thien Dinh <tdinhcs@gmail.com>
2018-09-01 13:47:04 -0400

    Added a Life of Pi picture.

--- app/index.js
+++ app/index.js
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+import clock from "clock";
+import document from "document";
+
+//Set when the clock activate itself.
+clock.granularity = "minutes";
+
+const timeLabel = document.getElementById("timeLabel");
+clock.ontick = (evt) => {
+	let today = evt.date;
+	let hours = today.getHours();
+	let mins = today.getMinutes();
+	timeLabel.text = `${hours}:${mins}`;
+}
